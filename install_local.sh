bdcb70efeeb0de78e691b477a15401bb9d440211
soda <soda@localhost.localdomain>
2020-08-20 20:41:16 +0530

    Updated the local install script for CentOS dev setup

--- install_local.sh
+++ install_local.sh
@@ -1,16 +1,3 @@
-# Check if FIGlet and Toilet are there and install if not.
-#if [ $(dpkg-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
-#then
-#  apt-get -y install figlet;
-#fi
-#if [ $(dpkg-query -W -f='${Status}' toilet 2>/dev/null | grep -c "ok installed") -eq 0 ];
-#then
-#  apt-get -y install toilet;
-#fi
-
-# Welcome Banner
-#figlet  -c -w 100  Welcome!
-
 echo "This script will setup a local development environment for working on the SODA Foundation documentation \n"
 echo "Please select the operating system that you are working on."
 echo "1. Ubuntu 16.04 / 18.04"
@@ -42,7 +29,7 @@ then
       printf "Download complete. \n"
       printf "Unpacking... \n"
       sudo dpkg -i hugo_0.55.6_Linux-64bit.deb &> /dev/null
-      printf "Unpack complete. \n"
+      printf "Unpacking complete. \n"
       printf "Installing Hugo... \n"
       sudo apt-get install -f &> /dev/null
       printf "Installation complete. \n"
@@ -68,5 +55,49 @@ then
   fi
 elif [ $os = '2' ]
 then
-    printf "Your OS: CentOS / RHEL \n"
+    sudo yum list --installed | grep redhat-lsb-core &> /dev/null
+    if [ $? = '0' ]
+    then 
+      printf "Your OS: \n"
+      lsb_release -a
+    elif [ $? = '1' ]
+    then
+      printf "Getting OS information... \n"
+      sudo yum install redhat-lsb-core -y &> /dev/null
+      printf "Your OS:\n"
+      lsb_release -a
+    fi
+
+    target="/usr/local/bin/hugo"
+    if find "$target" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+      printf "Hugo is installed \n"
+      hugo version
+    else
+      printf "Hugo is not installed \n"
+      printf "Downloading hugo_0.55.6_Linux-64bit.tar.gz ... \n"
+      wget https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz &> /dev/null
+      printf "Download complete. \n"
+      printf "Unpacking and installing... \n"
+      sudo tar -C /usr/local/bin/ -xvzf hugo_0.55.6_Linux-64bit.tar.gz &> /dev/null
+      printf "Unpack Hugo complete. \n"
+      printf "Installing Hugo complete. \n"
+      hugo version
+      printf "Cleaning up...\n"
+      printf "Deleting hugo_0.55.6_Linux-64bit.tar.gz ... \n"
+      rm hugo_0.55.6_Linux-64bit.tar.gz*
+      printf "Clean up complete. \n"
+      printf "\n\n"
+    fi
+    target="themes/hugo-theme-learn"
+    if find "$target" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+      echo "Learn theme is already setup. \n"
+      printf "You can run \'hugo serve \' to start the local server. \n"
+    else
+      echo "'$target' is empty or not a directory \n"
+      printf "Learn theme is not initiated. \n"
+      printf "Initiating the Learn Theme... \n"
+      git submodule update --init --recursive &> /dev/null
+      printf "Learn theme initiated. \n"
+      printf "You can run \'hugo serve \' to start the local server. \n"
+    fi
 fi
